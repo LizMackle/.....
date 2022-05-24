039498b73edf144ebf8ae4fe70464a54a56f78a4
LizMackle <liz.mackle@outlook.com>
2022-05-24 15:22:26 +0800

    merge after code broke

---
+++ src/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+    return (
+
+        <footer className="fixed-bottom py-2 bg-dark" >
+            {/* <p className="m-0 text-center text-white" id="footer-text">
+                Made with 🤍
+            </p> */}
+        </footer>
+    );
+}
+
+export default Footer;
